b885a0cd04e887acd2b8144bc6ad24e6973f94f6
Abdul Mutalib <mutalib92847@gmail.com>
2026-02-12 01:06:55 +0530

    create product.routes.js

---
+++ src/routes/product.routes.js
@@ -0,0 +1,37 @@
+import express from "express";
+import {
+  createProduct,
+  getAllProducts,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct,
+} from "../controllers/product.controller.js";
+
+import { authMiddleware } from "../middleware/authMiddleware.js";
+
+const productRouter = express.Router();
+
+/* ================= PUBLIC ROUTES ================= */
+productRouter.get("/", getAllProducts);
+productRouter.get("/:slug", getSingleProduct);
+
+/* ================= ADMIN ROUTES ================= */
+productRouter.post(
+  "/",
+  authMiddleware(["admin"]),
+  createProduct
+);
+
+productRouter.put(
+  "/:id",
+  authMiddleware(["admin"]),
+  updateProduct
+);
+
+productRouter.delete(
+  "/:id",
+  authMiddleware(["admin"]),
+  deleteProduct
+);
+
+export default productRouter;
